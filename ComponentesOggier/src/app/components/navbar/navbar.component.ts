8e9a5c8cb197f77202f6a6e0754f95187eaef37f
goggier <german.oggier19@gmail.com>
2022-03-17 11:05:29 -0300

    correccion varia

--- ComponentesOggier/src/app/components/navbar/navbar.component.ts
+++ ComponentesOggier/src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  boolInicio: boolean = true;
+  boolAgentes: boolean = true;
 
   constructor() { }
 
